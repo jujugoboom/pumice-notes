43be45a1e305fce1d5164f2315261f6393b2d989
Justin Covell <jujugoboom@gmail.com>
2022-04-17 17:55:35 -0700

    Add hyperclick tooltips

--- src/editor/extensions/hyperclick.js
+++ src/editor/extensions/hyperclick.js
@@ -1,6 +1,12 @@
+import { hoverTooltip } from "@codemirror/tooltip";
 import { EditorView } from "@codemirror/view";
 
 let openFile;
+
+const platform = window.navigator?.userAgentData?.platform ?? window.navigator.platform;
+const macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'];
+
+const isMac = macosPlatforms.indexOf(platform) !== -1;
 
 const handleClick = (view, event) => {
     if (event.ctrlKey || event.metaKey) {
@@ -20,8 +26,31 @@ const handleClick = (view, event) => {
     }
 }
 
+const hyperclickTooltip = hoverTooltip((view, pos, side) => {
+    const dom = view.domAtPos(pos);
+    if (dom.node?.parentElement?.classList?.contains('cm-url')) {
+        let {from, to, text} = view.state.doc.lineAt(pos);
+        let start = pos, end = pos
+        while (start > from && /\w/.test(text[start - from - 1])) start--
+        while (end < to && /\w/.test(text[end - from])) end++
+        if ((start === pos && side < 0) || (end === pos && side > 0))
+            return null
+        return {
+            pos: start,
+            end,
+            above: true,
+            create(view) {
+                let dom = document.createElement("div")
+                dom.textContent = `${isMac ? "Cmd" : "Ctrl"}+click to open`
+                return {dom}
+            }
+        }
+    }
+})
+
 export function hyperclick(config = {}) {
     openFile = config.openFile;
     const hyperclick = EditorView.mouseSelectionStyle.of(handleClick);
-    return [hyperclick];
+    
+    return [hyperclick, hyperclickTooltip];
 }
